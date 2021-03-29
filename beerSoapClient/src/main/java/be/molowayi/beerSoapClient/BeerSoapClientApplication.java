1b76d0aeb8e5e8d0ec492bbe6bf4701ad630ac41
Alex Molowayi <47976346+Molowayi@users.noreply.github.com>
2021-03-29 23:29:28 +0200

    Add files via upload
    ttt

--- beerSoapClient/src/main/java/be/molowayi/beerSoapClient/BeerSoapClientApplication.java
+++ beerSoapClient/src/main/java/be/molowayi/beerSoapClient/BeerSoapClientApplication.java
@@ -1,13 +1,36 @@
 package be.molowayi.beerSoapClient;
 
+import be.molowayi.spring.hello.client.HelloServiceEndpoint;
+import javafx.scene.Parent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.jaxws.JaxWsPortProxyFactoryBean;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @SpringBootApplication
 public class BeerSoapClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeerSoapClientApplication.class, args);
+		ConfigurableApplicationContext ctx = 	SpringApplication.run(BeerSoapClientApplication.class, args);
+
+		HelloServiceEndpoint helloService = ctx.getBean("hello", HelloServiceEndpoint.class );
+		String response = helloService.sayHello("World");
+		System.out.println(response);
 	}
 
+	@Bean
+	public JaxWsPortProxyFactoryBean hello() throws MalformedURLException {
+		JaxWsPortProxyFactoryBean proxy = new JaxWsPortProxyFactoryBean();
+		proxy.setServiceInterface(HelloServiceEndpoint.class);
+		proxy.setWsdlDocumentUrl(new URL("http://localhost:8079/HelloService?wsdl"));
+		proxy.setNamespaceUri("http://beerSoapService.molowayi.be/");
+		proxy.setServiceName("HelloService");
+		proxy.setPortName("HelloServiceEndpointPort");
+		return proxy;
+
+	}
 }
---
+++ beerSoapClient/src/main/java/be/molowayi/spring/hello/client/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://beerSoapService.molowayi.be/")
+package be.molowayi.spring.hello.client;
